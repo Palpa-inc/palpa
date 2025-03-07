aba1893e4b46f30c97504c2dceaa1ae37e8c74c9
mkitsugi <mkitsugi250@gmail.com>
2025-03-07 18:20:32 +0900

    Add CustomCursor and CreativeDotBackground components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import { CreativeDotBackground } from "@/components/creative-dot-background";
+import { CreativeDotBackground } from "@/components/creative-dot-background";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { HeroSection } from "@/components/sections/hero";
@@ -9,7 +9,7 @@ import { ContactSection } from "@/components/sections/contact";
 export default function Home() {
   return (
     <div className="relative">
-      {/* <CreativeDotBackground /> */}
+      <CreativeDotBackground />
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="container py-10">
